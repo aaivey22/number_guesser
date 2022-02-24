ef73bf00b6a12100b105a72bd4e7a4817eb582a1
aaivey22 <angelawings22@gmail.com>
2022-02-23 21:52:27 -0500

    changes to js: added event handler to .check to catch clicks and a func expression to display a message to user if a number is not entered when button is clicked.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🥳Correct Number!';
 
@@ -8,3 +9,13 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+// function expression inside of the event handler
+document.querySelector('.check').addEventListener
+('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = '❓No number!?'
+    }
+});
